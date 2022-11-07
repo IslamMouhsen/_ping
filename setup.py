7b6da83ac3b7bde8fce2fcbe1935cbfe11167ac1
Islam Mouhsen <islam.a.mouhsen@gmail.com>
2022-11-07 20:22:20 +0200

    First Release setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='_Ping',
+    version='0.1',
+    packages=['colorama'],
+    url='https://github.com/IslamMouhsen/_ping',
+    license='',
+    author='Islam Mouhsen',
+    author_email='islam.a.mouhsen@gmail.com',
+    description='_ping is script to manipulate window ping.exe and display pings with time stamp and colorized output based on ping time threshold  '
+)
